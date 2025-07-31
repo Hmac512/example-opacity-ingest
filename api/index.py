b5601b4128ede1a40fa294b9be159344e816c899
EulerLagrange.eth <76923506+Hmac512@users.noreply.github.com>
2025-07-30 20:30:52 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -65,4 +65,4 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(html_conten
+        self.wfile.write(html_content)
